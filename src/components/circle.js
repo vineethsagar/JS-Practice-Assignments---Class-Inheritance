6abba989e429e2faf813798ea949cb80e02f2721
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-11-19 12:14:50 +0530

    added circle and shape classes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,12 @@
 // import using require
-
+const Shape = require("./shape");
 // declare class
-
+class Circle extends Shape {
+  constructor() {}
+  calculateArea() {
+    console.log("override caluclate area");
+  }
+}
 
 // export class using module.exports
+module.exports = { Circle };
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {
+    console.log("draw shape");
+  }
+  calculateArea() {
+    console.log("calculate area");
+  }
+}
+module.exports = { Shape };
